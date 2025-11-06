5a3f14c0f91d1919539a3080d0d894493428ea88
Munier Salem <munier@Muniers-MacBook-Air.local>
2025-11-06 03:46:12 -0700

    prune

--- schemas/activitySchema.ts
+++ schemas/activitySchema.ts
@@ -6,7 +6,6 @@ export const activitySchema = z.object({
   timezone: z.string().min(1).default('America/Denver'),
   sport_type: z.string().min(1),
   distance: z.number().nonnegative(),
-  moving_time: z.number().nonnegative(),
   total_elevation_gain: z.number().nonnegative(),
   has_heartrate: z.boolean().default(false),
 
