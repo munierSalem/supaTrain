dc56de6590a53d763eac1717a9f828031892a3ea
Munier Salem <munier@Muniers-MacBook-Air.local>
2025-11-07 15:48:46 -0700

    sticky header on activity table

--- components/ActivityTable.tsx
+++ components/ActivityTable.tsx
@@ -22,59 +22,68 @@ export default function ActivityTable() {
   let prevWeek: string | null = null;
 
   return (
-    <table className="stats-table activity-table">
-      <thead>
-        <tr>
-          <th></th>
-          <th className="border-right">Date</th>
-          <th className="border-right">Sport</th>
-          <th>Moving Time</th>
-          <th>Dist (mi)</th>
-          <th>Vert (ft)</th>
-          <th  className="border-right">Max Elvt'n (ft)</th>
-          <th>Avg HR (bpm)</th>
-          <th>Max HR (bpm)</th>
-        </tr>
-      </thead>
-      <tbody>
-        {activities.map((a) => {
-          const isNewWeek = a.weekKey !== prevWeek;
-          const isClickable = ['strava'].includes(a.source);
-          prevWeek = a.weekKey;
+    <div className="activityTableContainer">
+      <table className="stats-table activity-table">
+        <thead>
+          <tr className="superheader">
+            <th colSpan="2" className="border-right"></th>
+            <th className="border-right"></th>
+            <th colSpan="2" className="border-right"></th>
+            <th colSpan="2" className="border-right">Elevation (ft)</th>
+            <th colSpan="2" className="border-right">Heartrate (bpm)</th>
+          </tr>
+          <tr>
+            <th></th>
+            <th className="border-right">Date</th>
+            <th className="border-right">Sport</th>
+            <th>Moving Time</th>
+            <th className="border-right">Dist (mi)</th>
+            <th>Gain</th>
+            <th  className="border-right">Max</th>
+            <th>Avg</th>
+            <th>Max</th>
+          </tr>
+        </thead>
+        <tbody>
+          {activities.map((a) => {
+            const isNewWeek = a.weekKey !== prevWeek;
+            const isClickable = ['strava'].includes(a.source);
+            prevWeek = a.weekKey;
 
-          return (
-            <tr
-              key={a.activity_id}
-              className={`activity-row ${isNewWeek ? 'new-week' : ''} ${isClickable ? 'clickable' : ''}`}
-              data-week={a.weekKey}
-              onClick={
-                a.source === 'strava'
-                  ? () => window.open(`https://www.strava.com/activities/${a.activity_id}`, '_blank', 'noopener,noreferrer')
-                  : undefined
-              }
-            >
-              <td>
-                {new Date(a.start_date).toLocaleDateString(undefined, {
-                  weekday: 'short'
-                })}
-              </td>
-              <td className="border-right">
-                {new Date(a.start_date).toLocaleDateString(undefined, {
-                  month: 'short',
-                  day: 'numeric',
-                })}
-              </td>
-              <td className="border-right">{a.sport_type}</td>
-              <td>{formatDuration(a.moving_time)}</td>
-              <td>{(a.distance / 1000 * 0.621371).toFixed(1)}</td>
-              <td>{Math.round(a.total_elevation_gain * 3.28084).toLocaleString()}</td>
-              <td  className="border-right">{Math.round(a.elev_high * 3.28084).toLocaleString()}</td>
-              <td>{(a.has_heartrate ? a.average_heartrate.toFixed(0) : '')}</td>
-              <td>{(a.has_heartrate ? a.max_heartrate.toFixed(0) : '')}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr
+                key={a.activity_id}
+                className={`activity-row ${isNewWeek ? 'new-week' : ''} ${isClickable ? 'clickable' : ''}`}
+                data-week={a.weekKey}
+                onClick={
+                  a.source === 'strava'
+                    ? () => window.open(`https://www.strava.com/activities/${a.activity_id}`, '_blank', 'noopener,noreferrer')
+                    : undefined
+                }
+              >
+                <td>
+                  {new Date(a.start_date).toLocaleDateString(undefined, {
+                    weekday: 'short'
+                  })}
+                </td>
+                <td className="border-right">
+                  {new Date(a.start_date).toLocaleDateString(undefined, {
+                    month: 'short',
+                    day: 'numeric',
+                  })}
+                </td>
+                <td className="border-right">{a.sport_type}</td>
+                <td>{formatDuration(a.moving_time)}</td>
+                <td className="border-right">{(a.distance / 1000 * 0.621371).toFixed(1)}</td>
+                <td>{Math.round(a.total_elevation_gain * 3.28084).toLocaleString()}</td>
+                <td  className="border-right">{Math.round(a.elev_high * 3.28084).toLocaleString()}</td>
+                <td>{(a.has_heartrate ? a.average_heartrate.toFixed(0) : '')}</td>
+                <td>{(a.has_heartrate ? a.max_heartrate.toFixed(0) : '')}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
