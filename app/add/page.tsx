25728b1c3ce536ecd3c42a64a79b6fc8e7ad318f
Munier Salem <munier@Muniers-MacBook-Air.local>
2025-11-07 16:08:53 -0700

    fix

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -2,9 +2,9 @@ import AddActivityForm from '@/components/AddActivityForm';
 
 export default function AddPage() {
   return (
-    <main>
+    <>
       <h2>Add Manual Activity</h2>
       <AddActivityForm />
-    </main>
+    </>
   );
 }
